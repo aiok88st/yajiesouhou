f5cf64d2891df6ab613f60eca962bc6a8013cd63
libinghe <804657038@qq.com>
2017-11-21 09:14:11 +0800

    123

--- runtime/temp/77549a195bca434bacf258e12188d8c7.php
+++ runtime/temp/77549a195bca434bacf258e12188d8c7.php
@@ -1,34 +1,53 @@
-{include file="common/head"/}
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:62:"/Users/Web/archie/yajiesouhou/app/admin/view/repair/index.html";i:1511226822;s:61:"/Users/Web/archie/yajiesouhou/app/admin/view/common/head.html";i:1511141992;s:61:"/Users/Web/archie/yajiesouhou/app/admin/view/common/foot.html";i:1511179449;}*/ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo config('sys_name'); ?>后台管理</title>
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" href="__STATIC__/plugins/layui/css/layui.css?v=1" media="all" />
+    <link rel="stylesheet" href="__ADMIN__/css/global.css?v=1" media="all">
+    <link rel="stylesheet" href="__STATIC__/common/css/font.css" media="all">
+</head>
+<body class="skin-0">
 <div class="admin-main layui-anim layui-anim-upbit">
     <fieldset class="layui-elem-field layui-field-title">
-        <legend>{:lang('list')}</legend>
+        <legend><?php echo lang('list'); ?></legend>
     </fieldset>
     <div class="layui-tab">
         <ul class="layui-tab-title">
-            {volist name="status_name" id="v"}
-            <li  onclick="window.location.href='{:url('repair/index',['status'=>$v['id']])}'"
-                    {if condition="$v['id'] eq $status"}class="layui-this"{/if}>
-                {$v['name']}
+            <?php if(is_array($status_name) || $status_name instanceof \think\Collection || $status_name instanceof \think\Paginator): $i = 0; $__LIST__ = $status_name;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$v): $mod = ($i % 2 );++$i;?>
+            <li  onclick="window.location.href='<?php echo url('repair/index',['status'=>$v['id']]); ?>'"
+                    <?php if($v['id'] == $status): ?>class="layui-this"<?php endif; ?>>
+                <?php echo $v['name']; ?>
             </li>
-            {/volist}
+            <?php endforeach; endif; else: echo "" ;endif; ?>
 
         </ul>
     </div>
 
     <div class="demoTable">
         <div class="layui-inline">
-            <input class="layui-input" name="key" id="key" placeholder="{:lang('pleaseEnter')}关键字">
+            <input class="layui-input" name="key" id="key" placeholder="<?php echo lang('pleaseEnter'); ?>关键字">
         </div>
-        <button class="layui-btn" id="search" data-type="reload">{:lang('search')}</button>
+        <button class="layui-btn" id="search" data-type="reload"><?php echo lang('search'); ?></button>
         <a href="javascript:;" onclick="window.location.reload()" class="layui-btn">显示全部</a>
         <button type="button" class="layui-btn layui-btn-danger" id="delAll">批量删除</button>
         <div style="clear: both;"></div>
     </div>
     <table class="layui-table" id="list" lay-filter="list"></table>
 </div>
-{include file="common/foot"/}
+<script type="text/javascript" src="__STATIC__/plugins/layui/layui.js"></script>
+<script type="text/javascript" src="__STATIC__/admin/js/common.js"></script>
+
+
 <script type="text/html" id="action">
-    <a href="{:url('repair/show')}?id={{d.id}}" class="layui-btn layui-btn-mini show">查看详情</a>
+    <a href="<?php echo url('repair/show'); ?>?id={{d.id}}" class="layui-btn layui-btn-mini show">查看详情</a>
     <a class="layui-btn layui-btn-danger layui-btn-mini" lay-event="del">删除</a>
 </script>
 <script>
@@ -38,13 +57,13 @@
         var tableIn = table.render({
             id: 'content',
             elem: '#list',
-            url: '{:url("repair/getList")}',
+            url: '<?php echo url("repair/getList"); ?>',
             method: 'get',
             page: true,
-            where:{status:'{$status}'},
+            where:{status:'<?php echo $status; ?>'},
             cols: [[
                 {checkbox: true, fixed: true},
-                {field: 'id', title: '{:lang("id")}', width: 80, fixed: true},
+                {field: 'id', title: '<?php echo lang("id"); ?>', width: 80, fixed: true},
                 {field: 'pro_id', title: '产品', width: 200,templet:"#pro_id"},
                 {field: 'name', title: '姓名', width: 120},
                 {field: 'phone', title: '电话', width: 200},
@@ -70,7 +89,7 @@
             if(obj.event === 'del'){
                 layer.confirm('您确定要删除该内容吗？', function(index){
                     var loading = layer.load(1, {shade: [0.1, '#fff']});
-                    $.post("{:url('repair/dele')}",{ids:data.id},function(res){
+                    $.post("<?php echo url('repair/dele'); ?>",{ids:data.id},function(res){
                         layer.close(loading);
                         if(res.code===1){
                             var name = $('#name').val();
@@ -97,7 +116,7 @@
                     ids.push(o.id);
                 });
                 var loading = layer.load(1, {shade: [0.1, '#fff']});
-                $.post("{:url('repair/dele')}", {ids: ids}, function (data) {
+                $.post("<?php echo url('repair/dele'); ?>", {ids: ids}, function (data) {
                     layer.close(loading);
                     if (data.code === 1) {
                         var name = $('#name').val();
